36e6afe4fc96370e3c32e9e330a84d38e4a0edce
jashvendra2001 <86592846+jashvendra2001@users.noreply.github.com>
2026-09-11 17:37:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-//your code here
-function handleLink()
-{
-	window.location.href="https://en.wikipedia.org/wiki/Golden_Retriever"
-}
+// //your code here
+// function handleLink()
+// {
+// 	window.location.href="https://en.wikipedia.org/wiki/Golden_Retriever"
+// }
 
-function handleLink1() 
-{
-	window.location.href="https://en.wikipedia.org/wiki/Poodle"
-}
+// function handleLink1() 
+// {
+// 	window.location.href="https://en.wikipedia.org/wiki/Poodle"
+// }
