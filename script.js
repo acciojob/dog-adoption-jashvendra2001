b17f0bf5f8c2fd231d77a1b8ce8a33d258bc6a3b
jashvendra2001 <86592846+jashvendra2001@users.noreply.github.com>
2026-09-11 17:29:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your code here
+function handleLink()
+{
+	window.location.href="https://en.wikipedia.org/wiki/Golden_Retriever"
+}
+
+function handleLink1() 
+{
+	window.location.href="https://en.wikipedia.org/wiki/Poodle"
+}
